9c50758ff120eb30c8cbeca0e6f71a236c401099
1 <1>
2018-06-05 17:33:43 +0800

    TEST

--- SAT/APP/UartSendData_task.c
+++ SAT/APP/UartSendData_task.c
@@ -4,6 +4,28 @@
 #define USARTSENDCANDATA 0
 UARTSENDDATA gUartSendData[USARTSENDBUF] = {{0},0};
 
+static void SwitchSendToCPUUartData(u8* src,u8* SendVal,u16 srclen)
+{
+    u16 i;
+     for(i = 0;i<srclen;i++)
+    {
+        src[2*i] = SendVal[i]>>4;
+        src[2*i+1] = SendVal[i]&0xf;
+    }
+      for(i = 0;i<srclen*2;i++)
+     {
+         if(src[i] < 10)
+         {
+             src[i] = src[i] + '0';
+         }
+         else
+         {
+             src[i] = src[i] + 0x37;
+         }
+     }   
+
+}
+
 void ATCommSendCAN(u8 *buf)
 {
 //	char Head[] = ""; 
