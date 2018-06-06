5ae0e85c80109fadb4fe4fbdf4071066baab7b1a
1 <1>
2018-06-06 17:00:04 +0800

    准备把串口发送的数据改成16bit发送，但是发现不能发送16bit，只能一次发送8bit数据！

--- SAT/APP/UartSendData_task.c
+++ SAT/APP/UartSendData_task.c
@@ -4,8 +4,8 @@
 #define USARTSENDCANDATA 0
 
 UARTSENDDATA gUartSendData[USARTSENDBUF] = {0};
-
-static void SwitchSendToCPUUartData(u8* src ,u8* SendVal,u16 srclen)
+#if 1
+static void SwitchCANData(u8* src ,u8* SendVal,u16 srclen)
 {
 	u16 i;
 	for(i = 0; i < srclen; i++)
@@ -31,10 +31,66 @@ static void SwitchSendToCPUUartData(u8* src ,u8* SendVal,u16 srclen)
 	}   
 
 }
+#else
+static void SwitchCANData(u16* src ,u8* SendVal,u16 srclen)
+{
+	u16 i;
+	u8 Tempbuf[40] = {0};
+	
+	for(i = 0; i < srclen; i++)
+	{
+		Tempbuf[2*i] = SendVal[i] >> 4;
+		Tempbuf[2*i+1] = SendVal[i] & 0xf;
+	}
+#if 0
+	printf("tempbuf:");
+	for(i = 0;i<40;i++)
+	{
+		printf("%02x ",Tempbuf[i]);
+	}
+	printf("\r\n");
+	
+	printf("src:");
+#endif
+	for(i = 0; i< srclen ; i++)
+	{
+		if (Tempbuf[2*i+1] <= 0x09)
+		{
+			src[i] = Tempbuf[2*i+1] + '0';
+		}
+		else if((Tempbuf[2*i+1] >= 0x0a) && (Tempbuf[2*i+1] <= 0x0f))
+		{
+			src[i] = Tempbuf[2*i+1] + 0x37;
+		}
+		else
+		{
+			src[i] = 0xFF;
+		}
 
+		
+		if (Tempbuf[2*i] <= 0x09)
+		{
+			src[i] |= ((u16)(Tempbuf[2*i] + '0')) << 8;
+		}
+		else if((Tempbuf[2*i] >= 0x0a) && (Tempbuf[2*i] <= 0x0f))
+		{
+			src[i] |= ((u16)(Tempbuf[2*i] + 0x37)) << 8;
+		}
+		else
+		{
+			src[i] |= 0xFF00;
+		}
+		//printf("|%02x,%02x|",Tempbuf[i],Tempbuf[i+1]);
+		//src[i] = (((u16)Tempbuf[i+1]) << 8 | Tempbuf[i]);
+		//printf("%0x ",src[i]);
+	}   
+	//printf("\r\n");
+}
+
+#endif
 void ATCommSendCAN(void)
 {
-	u16 i = 0;
+//	u16 i = 0;
 	static u8 CanRxCount = 0;
 	if(gCanRxRawDataBuf[CanRxCount].NewDataFlag == 1)
 	{
@@ -49,7 +105,7 @@ void ATCommSendCAN(void)
 		}
 		printf("\r\n");
 		#endif
-		SwitchSendToCPUUartData(gUartSendData[USARTSENDCANDATA].UartSendBuf,gCanRxRawDataBuf[CanRxCount].Buf, 
+		SwitchCANData(gUartSendData[USARTSENDCANDATA].UartSendBuf,gCanRxRawDataBuf[CanRxCount].Buf, 
 							USART_SEND_LEN);
 		
 		memset(gCanRxRawDataBuf[CanRxCount].Buf, 0, RXMSG_LEN);
